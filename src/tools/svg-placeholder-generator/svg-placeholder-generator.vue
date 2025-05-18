653a493dd8dec61d907f019b6b55749d15fd39d7
myself <donghe587@139.com>
2025-05-18 22:49:08 +0800

    fix(components): 修复组件类型定义和工具页面内容
    - 修正 Ipv4AddressConverter 组件的 Portuguese 语言文件路径
    - 更新工具页面的 Markdown 文件引用，包括 svg-placeholder-generator 和 yaml-to-json-converter
    - 在 svg-placeholder-generator 页面添加 Markdown 内容显示

--- src/tools/svg-placeholder-generator/svg-placeholder-generator.vue
+++ src/tools/svg-placeholder-generator/svg-placeholder-generator.vue
@@ -7,7 +7,7 @@ import showdown from 'showdown'; // 新增showdown引入
 const { t, locale } = useI18n();
 const markdownHtml = ref('');
 const loadMarkdown = async () => {
-  const mdContent = await import(`./language/token-generator.${locale.value}.md?raw`);
+  const mdContent = await import(`./language/svg-placeholder-generator.${locale.value}.md?raw`);
   const converter = new showdown.Converter();
   markdownHtml.value = converter.makeHtml(mdContent.default);
 };
@@ -99,8 +99,10 @@ const { download } = useDownloadFileFromBase64({ source: base64 });
       </c-button>
     </div>
   </div>
-
   <img :src="base64" alt="Image">
+  <c-card>
+    <div v-html="markdownHtml"></div>
+  </c-card>
 </template>
 
 <style lang="less" scoped>
--- src/tools/yaml-to-json-converter/yaml-to-json.vue
+++ src/tools/yaml-to-json-converter/yaml-to-json.vue
@@ -7,7 +7,7 @@ import showdown from 'showdown'; // 新增showdown引入
 const { t, locale } = useI18n();
 const markdownHtml = ref('');
 const loadMarkdown = async () => {
-  const mdContent = await import(`./language/yaml-to-json.${locale.value}.md?raw`);
+  const mdContent = await import(`./language/yaml-to-json-converter.${locale.value}.md?raw`);
   const converter = new showdown.Converter();
   markdownHtml.value = converter.makeHtml(mdContent.default);
 };
